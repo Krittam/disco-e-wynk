879000c083669aedff1be75747b175c497c7471f
B0212180 <B0212180@LTB0212180-MAC.local>
2019-07-26 18:25:35 +0530

    fetching song objects

--- app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
+++ app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.util.logging.Logger;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Main_Activity";
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -31,14 +43,49 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getContentFromApi();
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
+
+    public void getContentFromApi(){
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getSearchResults("dilbar")
+                .enqueue(new Callback<SearchResponse>() {
+
+                    @Override
+                    public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                        SearchResponse searchResponse = response.body();
+                        Item songItem = null;
+                        for (Item item : searchResponse.getItems()){
+                            if ("SONG".equals(item.getId())){
+                                songItem = item;
+                                break;
+                            }
+                        }
+                        songItem.getItems();
+
+                    }
+
+
+                    @Override
+                    public void onFailure(Call<SearchResponse> call, Throwable t) {
+                        Log.d(TAG, t.getLocalizedMessage());
+                    }
+                });
+    }
+
+
+
+
 
 }
