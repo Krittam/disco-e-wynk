a2cbf7bcc2271c6d0f7427880172de3d9873ee67
= <divyanshjain22@gmail.com>
2019-07-26 22:33:12 +0530

    search implement

--- app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
+++ app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
@@ -1,44 +1,71 @@
 package in.wynk.disco.disco_e_wynk;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter< String > adapter;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context=this;
+        searchView = (SearchView) findViewById(R.id.searchView);
+        listView = (ListView) findViewById(R.id.lv1);
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(true){
+                    Log.d("tag", "hey");
+                    list.add("dilar2");
+                    list.add("dilbar3");
+                    list.add("dilbar3");
+                    list.add("dilbar3");
+                    adapter.notifyDataSetChanged();
+                    Log.d("tag", query);
+                }else{
+                    Toast.makeText(MainActivity.this, "No Match found",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                //TODO implement song enque
+                Log.d("tag", Integer.toString(position));
+
+                list.clear();
+                searchView.setQuery("", false);
+                searchView.clearFocus();
+                adapter.notifyDataSetInvalidated();
+            }
+        });
     }
-
 }
