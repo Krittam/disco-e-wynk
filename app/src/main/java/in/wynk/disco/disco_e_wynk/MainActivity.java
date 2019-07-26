1f1dc8770f0d53c0d30933617a3a3d9b7010a4db
krittamk <krittam.kothari@wynk.in>
2019-07-26 18:36:15 +0530

    implement firebase; deeplink

--- app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
+++ app/src/main/java/in/wynk/disco/disco_e_wynk/MainActivity.java
@@ -1,33 +1,36 @@
 package in.wynk.disco.disco_e_wynk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
+    private Button.OnClickListener mOnHostClickListener = new View.OnClickListener() {
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
+        public void onClick(View view) {
+            Intent myIntent = new Intent(MainActivity.this, PartyActivity.class);
+            Bundle extras = new Bundle();
+            extras.putBoolean("isHost",true);
+            myIntent.putExtras(extras);
+            MainActivity.this.startActivity(myIntent);
         }
     };
 
@@ -35,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String userId = "krittam_kothari";
+
+        View button = findViewById(R.id.host_button);
+        button.setOnClickListener(mOnHostClickListener);
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
